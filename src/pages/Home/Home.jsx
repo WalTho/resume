c1a15d1fd3d0e864d906451bb3b0692bfbff8de7
Thomas F <waltorias.as@gmail.com>
2024-05-31 12:40:36 +0200

    feat: languages, print buttons and responsive WIP

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,91 +1,93 @@
+import React, { useState } from 'react';
 import Photo from '../../assets/profile.png';
-
+import UK_Flag from '../../assets/Flag_of_the_UK.svg';
+import FR_Flag from '../../assets/Flag_of_France.svg';
 import './home.scss';
+import enLang from '../../json/en-lang.json';
+import frLang from '../../json/fr-lang.json';
 
 const Home = () => {
+    const [lang, setLang] = useState(enLang);
+
+    const switchLanguage = () => {
+        setLang(lang === enLang ? frLang : enLang);
+    };
+    const printPage = () => {
+        window.print();
+    };
+
     return (
         <section className='home'>
+            <button onClick={switchLanguage} className='home__lang-switch' title={lang.lang.title}>
+                <img className='home__lang-switch-img' src={lang === enLang ? FR_Flag : UK_Flag} alt={lang === enLang ? 'French Flag' : 'UK Flag'} />
+            </button>
+            <button onClick={printPage} className='home__print-button' title={lang.print.title}>
+                <i className='fas fa-print'></i>
+            </button>
             <article className='home__profile'>
                 <div className='home__profile-photo'>
                     <img className='home__profile-img' src={Photo} alt="profile-photo" />
                 </div>
                 <div className='home__profile-info'>
-                    <h1 className='home__profile-info-name'>
-                        THOMAS <br /><strong>FLAMENT</strong>
-                    </h1>
-                    <h2 className='home__profile-info-title'>
-                        UI/UX & Full Stack Web Developer
-                    </h2>
+                    <h1 className='home__profile-info-name' dangerouslySetInnerHTML={{ __html: lang.profile.name }} />
+                    <h2 className='home__profile-info-title'>{lang.profile.title}</h2>
                 </div>
                 <div className='home__profile-contact'>
                     <span>
-                        <a href='https://maps.app.goo.gl/wjmwPXbfxwC1dP3H7' target='_blank' className='home__profile-contact-adress'>120 Bd Clémenceau, <br /> 59700 Marcq-en-Baroeul</a>
+                        <a href='https://maps.app.goo.gl/wjmwPXbfxwC1dP3H7' target='_blank' className='home__profile-contact-adress'>
+                            {lang.contact.address}
+                        </a>
                     </span>
                     <span>
-                        <a href='tel:+33641437470' className='home__profile-contact-phone' >
-                            +33641437470
+                        <a href='tel:+33641437470' className='home__profile-contact-phone'>
+                            {lang.contact.phone}
                             <i className="fa-solid fa-phone"></i>
                         </a>
                     </span>
                     <span>
                         <a href='mailto:tflament@icloud.com' className='home__profile-contact-email'>
-                            tflament@icloud.com
+                            {lang.contact.email}
                             <i className="fa-solid fa-envelope"></i>
                         </a>
-
                     </span>
                 </div>
             </article>
             <article className='home__about'>
-                <h2 className='home__about-title'>ABOUT</h2>
-                {/* <p className='home__about-text'>
-                    Je suis un développeur fullstack passionné, spécialisé dans le développement frontend UI/UX, avec une solide expérience dans la conception et la mise en œuvre d'APIs, le développement backend, ainsi que l'intégration de systèmes complexes. <br />
-                    J'aime créer des solutions technologiques qui offrent une expérience utilisateur exceptionnelle. <br />
-                    Je suis également compétent dans la coordination avec les équipes backend pour intégrer harmonieusement les interfaces utilisateur avec les fonctionnalités backend. <br />
-                    Ma capacité à comprendre les besoins des utilisateurs et à traduire ces besoins en solutions techniques efficaces fait de moi un atout précieux pour tout projet de développement web. <br />
-                    Je suis également familier avec les méthodologies Agile telles que SCRUM et KANBAN, ce qui me permet de travailler de manière collaborative et efficace.
-                </p> */}
-                <p className='home__about-text'>
-                    I am a passionate fullstack developer, specializing in frontend UI/UX development, with a strong background in designing and implementing APIs, backend development, as well as integrating complex systems. <br />
-                    I enjoy crafting technological solutions that provide exceptional user experiences. <br />
-                    Additionally, I am proficient in collaborating with backend teams to seamlessly integrate user interfaces with backend functionalities. <br />
-                    My ability to grasp user needs and translate them into efficient technical solutions makes me a valuable asset for any web development project. <br />
-                    I am also familiar with Agile methodologies such as SCRUM and KANBAN, enabling me to work collaboratively and efficiently.
-                </p>
+                <h2 className='home__about-title'>{lang.about.title}</h2>
+                <p className='home__about-text' dangerouslySetInnerHTML={{ __html: lang.about.text }} />
             </article>
             <article className='home__skills'>
-                <h2 className='home__skills-title'>SKILLS</h2>
+                <h2 className='home__skills-title'>{lang.skills.title}</h2>
                 <ul className='home__skills-text'>
-                    <li><h3>Languages:</h3> HTML5, CSS3, SASS, JavaScript, ReactJS, Thymeleaf, Java</li>
-                    <li><h3>Tools:</h3> IntelliJ, Visual Studio Code, PSQL, MySQL, PostMan, Docker, Figma, Framer</li>
-                    <li><h3>Methodologies:</h3> SCRUM, KANBAN</li>
-                    <li><h3>Version Control:</h3> Git/GitHub</li>
-                    <li><h3>Others:</h3> Development best practices, technology watch, naming conventions, Micro-feature work.</li>
+                    <li><h3>{lang.skills.items.languages.split(": ")[0]}:</h3> {lang.skills.items.languages.split(": ")[1]}</li>
+                    <li><h3>{lang.skills.items.tools.split(": ")[0]}:</h3> {lang.skills.items.tools.split(": ")[1]}</li>
+                    <li><h3>{lang.skills.items.methodologies.split(": ")[0]}:</h3> {lang.skills.items.methodologies.split(": ")[1]}</li>
+                    <li><h3>{lang.skills.items.versionControl.split(": ")[0]}:</h3> {lang.skills.items.versionControl.split(": ")[1]}</li>
+                    <li><h3>{lang.skills.items.others.split(": ")[0]}:</h3> {lang.skills.items.others.split(": ")[1]}</li>
                 </ul>
             </article>
             <article className='home__expertise'>
-                <h2 className='home__expertise-title'>MY LAST EXPERIENCES</h2>
+                <h2 className='home__expertise-title'>{lang.expertise.title}</h2>
                 <div className='home__expertise-text'>
-                    <h3>FROM 03/03/2023 TO 25/12/2023</h3>
-                    <p><strong>POSITION:</strong> Fullstack Developer on assignment</p>
-                    <p><strong>COMPANY:</strong> EkeepIT - Client: MATCH</p>
-                    <p>I developed an API for a connected cash register, ensuring compatibility with current and future systems through the hexagonal architecture. Implemented in JAVA 11, this approach prioritized robustness, maintainability, and scalability. Additionally, I optimized the frontend  in ReactJS, enhancing performance by minimizing file size and resource usage.</p>
-
-                    <h3>FROM 03/01/2021 TO 03/2023</h3>
-                    <p><strong>POSITION:</strong> Web Developer</p>
-                    <p><strong>COMPANY:</strong> EkeepIT</p>
-                    <p>Taking on the role of integrator and Frontend developer in 2 incubation projects:</p>
-                    <a href='https://my-uuu.com/'>- <strong>My-UUU:</strong></a>
-                    <p>In a team of five, I contributed as a Frontend/Integrator to a website facilitating equestrian center registration and user search based on specific criteria. I focused on UI/UX enhancements, integrating APIs in ReactJS, converting mockups into functional pages.</p>
-                    <a href='https://my-actividay.com/'>- <strong>My-Actividay:</strong></a>
-                    <p>In a team of six, I served as a Frontend/Integrator for a web app providing CRA creation services to micro-enterprises and SMEs. I focused on UI/UX improvements, including mockup conversion to functional pages, SCSS animations, and project structuring using xHTML5 and Thymeleaf.</p>
-
+                    {lang.expertise.experiences.map((experience, index) => (
+                        <div key={index}>
+                            <h3>{experience.date}</h3>
+                            <p><strong>{lang.expertise.position}</strong> {experience.position}</p>
+                            <p><strong>{lang.expertise.company}</strong> {experience.company}</p>
+                            <p>{experience.description}</p>
+                            {experience.projects && experience.projects.map((project, idx) => (
+                                <React.Fragment key={idx}>
+                                    <a href={project.url}>- <strong>{project.name}:</strong></a>
+                                    <p>{project.description}</p>
+                                </React.Fragment>
+                            ))}
+                        </div>
+                    ))}
                 </div>
             </article>
-
             <article className='home__more'>
-                <h2 className='home__more-title'>MORE ABOUT ME</h2>
-                <p className='home__more-text'>In my spare time, I am actively involved in creating small projects. Notably, I developed the website for <a href="https://www.plasticvortex.co/">Plastic Vortex</a>, an organization dedicated to protecting river and ocean ecosystems by mitigating plastic pollution. I have a strong appetite for learning and am constantly exploring new topics and technologies to expand my skills and knowledge.</p>
+                <h2 className='home__more-title'>{lang.more.title}</h2>
+                <p className='home__more-text' dangerouslySetInnerHTML={{ __html: lang.more.text }} />
             </article>
         </section>
     );
